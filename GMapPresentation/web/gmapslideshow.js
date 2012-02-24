4a4b10f711ef74f3a98678024caf98fe4d1543d7
Dan Martin <dmartin@dmartin.org>
2012-02-24 10:25:01 -0600

    Initial check in.

---
+++ GMapPresentation/web/gmapslideshow.js
@@ -0,0 +1,124 @@
+var map;
+    	
+    	var changingSlides = false;
+    	
+        var currentSlideId = -1;
+        var infoWindows = [];
+        
+        function showNextSlide() {
+        	if (currentSlideId == (slides.length - 1)) {
+   				showSlide(0);
+    		} else {
+    			showSlide(currentSlideId + 1);
+    		}
+        }
+        
+        function showPreviousSlide() {
+        	if (currentSlideId > 0) {
+       			showSlide(currentSlideId - 1);
+        	} else {
+        		showSlide(slides.length - 1);
+        	}
+        }
+        
+        function showSlide(slideId) {
+        	if (!changingSlides) {
+	        	changingSlides = true;
+	        	var slide = document.getElementById("slide" + currentSlideId);
+	        	if (slide) {
+	        		slide.className = "";
+	        	}
+	        	
+	        	if (infoWindows[currentSlideId]) {
+		        	infoWindows[currentSlideId].close(map);
+	        	}
+	        	
+	        	if (slideId >= 0 && slideId < slides.length) {
+	        		currentSlideId = slideId;
+	        		doZoomOut();
+	        	}
+	        	
+	        	document.getElementById("slide" + currentSlideId).className = "activeSlide";
+        	}
+        }
+        
+        function doZoomOut() {
+        	if (map.getZoom() > 2) {
+        		map.setZoom(map.getZoom() - 1);
+        		setTimeout("doZoomOut()", 100);
+        	} else {
+        		setTimeout("doPan()", 1000);
+        	}
+    		
+        }
+        
+        function doPan() {
+        	var slide = slides[currentSlideId];
+        	map.panTo(slide.point);
+        	doZoomIn();
+        }
+        
+        function doZoomIn() {
+        	var slide = slides[currentSlideId];
+        	if (map.getZoom() < slide.zoom) {
+        		map.setZoom(map.getZoom() + 1);
+        		setTimeout("doZoomIn()", 100);
+ 	       } else {
+        		infoWindows[currentSlideId].open(map);
+        		changingSlides = false;
+ 	       }
+        }
+        
+        function initialize() {
+            var myOptions = {
+              center: new google.maps.LatLng(0,0),
+              zoom: 3,
+              disableDefaultUI: true,
+              mapTypeId: google.maps.MapTypeId.SATELLITE
+            };
+            map = new google.maps.Map(document.getElementById("map_canvas"),
+                myOptions);
+            
+            setupInfoWindows();
+			setupSlideList();
+			setTimeout("showNextSlide()", 3000);
+            
+            document.onkeyup = onKeyPress;
+      	}
+        
+        function setupInfoWindows() {
+        	for (var i = 0; i < slides.length; i++) {
+            	var infoWindow = new google.maps.InfoWindow();
+            	infoWindow.setContent("<div class='infowindow'><h1>" + slides[i].title + "</h1>" + slides[i].contents + "</div>");
+            	infoWindow.setPosition(slides[i].point);
+            	infoWindows[i] = infoWindow;
+        	}
+        }
+        
+        function setupSlideList() {
+        	var slideList = document.getElementById("slideList");
+			
+        	for (var i = 0; i < slides.length; i++) {
+        		var child = document.createElement('li');
+        		
+        		(function (i) {
+        		    child.addEventListener("click", function() { 
+        		      showSlide(i);
+        		    }); 
+        		  })(i);
+        		
+            	child.innerHTML = "<p><em>" + slides[i].title + "</em></p>"; 
+            	child.setAttribute("id", "slide" + i);
+            	slideList.appendChild(child);
+            	
+        	}
+        	
+        }
+        
+        function onKeyPress(event) {
+        	if (event.keyCode == 40 || event.keyCode == 34 || event.keyCode == 39 || event.keyCode == 32) {
+        		showNextSlide();
+        	} else if (event.keyCode == 38 || event.keyCode == 33 || event.keyCode == 37) {
+        		showPreviousSlide();
+        	}
+        }
---
+++ GMapPresentation/web/slides.js
@@ -0,0 +1,260 @@
+var slides = [
+           {
+        	   title: "Overview",
+        	   contents: 
+        		   "<ul>" +
+        	   		  "<li>History of Open</li>" +
+        	   		  "<li>Characteristics of Open</li>" +
+        	   		  "<li>Impact of Open</li>" +
+        	   		  "<li>MasterCard's OpenAPI</li>" +
+        	   		  "<li>Future of Open</li>" +
+    	   		   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(32.95982, -96.64095)
+        	   
+           },
+           {
+        	   title: "History of Open Source",
+        	   contents: 
+        		   "<ul>" +
+        	   		  "<li>Books</li>" +
+        	   		  "<li>WWIV BBS - 1980s</li>" +
+        	   		  "<li>Invention of GPL and Free Software</li>" +
+        	   		  "<li>Usenet</li>" +
+        	   		  "<li>Linux</li>" +
+        	   		  "<li>Apache</li>" +
+        	   		  "<li>Everything else</li>" +
+        	   	   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(45.123836, -123.113040)
+        	   
+           },
+           {
+        	   title: "History of Open APIs",
+        	   contents: 
+        		   "<ul>" +
+        	   		  "<li>EBay - tools for power sellers</li>" +
+        	   		  "<li>Amazon - affiliate apis</li>" +
+        	   		  "<li>Salesforce - Enterprise SAAS</li>" +
+        	   		  "<li>Google - maps</li>" +
+        	   		  "<li>Social explosion led by Facebook</li>" +
+        	   		  "<li>Everyone else - exponential growth</li>" +
+        	   	   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(37.37732, -121.92151)
+        	   
+           },
+           {
+        	   title: "Characteristics of Open Source and Open APIs",
+        	   contents: 
+        		   "<h4>Differences</h4>" +
+        		   "<ul>" +
+	        		   "<li>Ethos, in particular with Free Software</li>" + 
+	        		   "<li>Level of open</li>" +
+	        	   "</ul>" +
+	        	   "<h4>Commonalities</h4>" +
+	        	   "<ul>" +
+	        		   "<li>Huge reach</li>" +
+	        		   "<li>Huge impact</li>" +
+	        		   "<li>Extremely low barrier to entry</li>" +
+	        		   "<li>Cost of distribution : creative business models required</li>" +
+        		   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(36.11302, -115.17305)
+           },
+           {
+        	   title: "The Impact of Open",
+        	   contents: 
+        		   "<ul><li>Open APIs and OSS: offering billions worth of tech to anyone who can code</li>" +
+	        		   "<li>Developers make the choice</li>" +
+	        		   "<li>Survival of the fittest not best salesman</li>" +
+	        		   "<li>Open feeds itself - The more you build, the more can be built</li>" +
+        		   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(48.874104, 2.294965)
+           },
+           {
+        	   title: "The Impact of Open Source Software",
+        	   contents: 
+        		   "<ul>" +
+	        		   "<li>Hard to quantify</li>" +
+	        		   "<li>Data center technology</li>" +
+	        		   "<li>Financial industries</li>" +
+	        		   "<li>Office software</li>" +
+	        		   "<li>Government operations</li>" +
+	        		   "<li>Mobile</li>" +
+	        		   "<li>Publishing</li>" +
+        		   "</ul>",
+        	   zoom: 18,
+        	   point: new google.maps.LatLng(48.87415, 2.29496)
+           },
+           {
+        	   title: "The Impact of Open Services",
+        	   contents: 
+        		   "<ul>" +
+	        		   "<li>GIS (Google Maps)</li>" +
+	        		   "<li>Payments (PayPal, MasterCard, Visa, Stripe)</li>" +
+	        		   "<li>E-commerce (majority of EBay listings via API)</li>" +
+	        		   "<li>The business of relationships (social)</li>" +
+	        		   "<li>Media distribution (twitter)</li>" +
+	        		   "<li>Data Center technology (Amazon)</li>" +
+	        		   "<li>Enterprise Software (Salesforce)</li>" +
+        		   "</ul>",
+        	   zoom: 12,
+        	   point: new google.maps.LatLng(38.798, -36.785)
+           },
+           {
+        	   title: "MasterCard's Open APIs",
+        	   contents: 
+        		   "<h4>http://developer.mastercard.com</h4>" +
+        		   "<img src='slidedata/openapi-homepage.png' />",
+        	   zoom: 12,
+        	   point: new google.maps.LatLng(38.798, -36.785)
+           },
+           {
+        	   title: "MasterCard's Services",
+        	   contents: 
+        		   "<h4>http://developer.mastercard.com</h4>" +
+        		   "Payments, Offers, Locations, MoneySend, and MATCH" +
+        		   "<img src='slidedata/openapi-services.png' />",
+        	   zoom: 12,
+        	   point: new google.maps.LatLng(41.02363, -73.71513)
+           },
+           {
+        	   title: "MasterCard's Future Services",
+        	   contents: 
+        		   "<ul>" +
+        		   		"<li>Fraud</li>" +
+        		   		"<li>Analytics</li>" +
+        		   		"<li>Advanced Payments, Social Payments, Recurring Payments</li>" +
+        		   		"<li>Seperation of Merchant/Developer Relationship</li>" +
+        		   "</ul>",
+        	   zoom: 10,
+        	   point: new google.maps.LatLng(41.02363, -73.71513)
+           },
+           {
+        	   title: "Beyond Open Source and Open Services",
+        	   contents: 
+        		   "The ideas of Open are spreading beyond software" +
+        		   "<ul>" +
+        		   		"<li><b>Open Knowledge</b> (Wikipedia, Wikileaks) - disrupting traditional knowledge industry, governments etc</li>" +
+        		   		"<li><b>Open Communication</b> - disrupting governments (twitter)</li>" + 
+        		   		"<li><b>Creative Commons</b> - opening content works, potential for disruption - I believe there's still more to come here</li>" +
+        		   		"<li><b>Open Publishing</b> - huge potential</li>" +
+        		   		"<li><b>Open Music</b> - future is still unclear</li>" +
+        		   		"<li><b>Open Government</b> - making all government data open.  Tim OReilly big proponent.  Impact can be huge, like the Census Bureau's open map data.</li>" +
+        		   	"</ul>",
+        	   zoom: 10,
+        	   point: new google.maps.LatLng(38.798, -36.785)
+           },
+           {
+        	   title: "The Future of Open",
+        	   contents: 
+        		   "Improved technology leading the way<br>" +
+        		   "<h4>For Open Source</h4>" +
+        		   "<ul>" +
+        		   		"<li>DVCS</li>" +
+        		   		"<li>Open Source feeding Open Source</li>" +
+        		   		"<li>Patent Commons</li>" +
+        		   		"<li>Repeats of Android</li>" +
+        		   		"<li>Mobile-Social-Cloud</li>" +
+	        	   "</ul>" +
+	        	   "<h4>For Open Services</h4>" +
+	        	   "<ul>" +
+        		   		"<li>Technologies like OAuth and OpenID</li>" +
+        		   		"<li>Open APIs major building block for apps</li>" +
+        		   		"<li>Social APIs a major force</li>" +
+        		   		"<li>Clearly immature - growth is exponential</li>" +
+        		   		"<li>Moving to REST</li>" +
+        		   		"<li>More use of formats other than XML > JSon, protobufs, etc</li>" +
+        		   		"<li>More tooling</li>" +
+        		   		"<li>Formation of an industry around APIs (tool providers, consultancies, etc.)</li>" +
+        		   		"<li>More app stores</li>" +
+        		   "</ul>",
+        	   zoom: 10,
+        	   point: new google.maps.LatLng(28.58707, -80.65274)
+           },
+        ];		
+
+
+/*
+
+
+History of Open
+Remember getting books from the library with game source code for basic.
+
+
+Definitions
+Open Source
+Open API - a service using open standards to make it's technology available to every developer in the world
+
+
+
+
+Different characteristics of OSS and Open APIs:
+Ethos, in particular with Free Software
+Level of open
+
+
+Characteristics of Open
+Huge reach
+Huge impact
+Extremely low barrier to entry
+Cost of distribution : creative business models required - fine line to walk
+
+
+
+
+The huge impact of Open:
+Open APIs and OSS are both about offering billions of dollars worth of technology to everyone on the planet who is capable of writing code.
+Developers make the choice, not salesman selling to management. -> Survival of the fittest not best salesman.
+Open feeds itself - The more you build, the more can be built.
+
+
+
+
+Places Open Source is impacting:
+It's hard to identify the complete impact.  Almost every product built today utilizes some OSS.
+Nearly everything, including:
+Data center technology
+Financial industries
+Office software
+Government operations
+Mobile
+Publishing
+
+
+
+
+Places Open APIs are impacting:
+GIS
+Payments
+The business of relationships (social)
+Media distribution (twitter)
+Data Center technology (Amazon web services)
+
+
+Future of OSS and Open APIs:
+Improved tech leading the way -
+OSS: 
+DVCS
+OSS feeding OSS
+Technologies like OAuth, OpenID
+
+
+
+
+Beyond OSS and OpenAPIs:
+The ideas of Open are spreading beyond software
+Open Knowledge (Wikipedia, Wikileaks) - disrupting traditional knowledge industry, governments etc
+Open Communication - disrupting governments (twitter)
+Creative Commons - opening content works, potential for disruption - I believe there's still more to come here
+Open Publishing - huge potential
+Open Music - future is still unclear
+Open Government - making all government data open.  Tim OReilly big proponent.  Impact can be huge, like the Census Bureau's open map data.
+
+
+
+ * 
+ */
+
